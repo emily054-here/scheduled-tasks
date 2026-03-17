7735569bfedaa04778e3964eb401e87aa799901a
emily054-here <ruihan054@gmail.com>
2026-03-17 10:48:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
@@ -20,19 +19,29 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+
+birthdays_dict = {
+    (data_row["month"], data_row["day"]): data_row
+    for (index, data_row) in data.iterrows()
+}
+
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
+
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    # 🔥 NEW PART
+    email_key = birthday_person["email"]
+    to_email = os.environ.get(email_key)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
+            to_addrs=to_email,
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
